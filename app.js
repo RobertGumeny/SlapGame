fb21e866815348503be86376f28441f7112bf8c2
Robert Gumeny <robert.gumeny528@gmail.com>
2020-03-19 13:33:14 -0600

    init

---
+++ app.js
@@ -0,0 +1,32 @@
+// GLOBAL VARIABLES/OBJECTS FOR PLAYER AND COMPUTER HEALTH/ATTACKS
+
+let player = {
+  health: 100,
+  attacks: {'slap': 10,
+            'kick': 15,
+            'punch': 20}
+}
+let computer = {
+  health: 100,
+  attacks: {'slap': 10,
+            'kick': 15,
+            'punch': 20}
+}
+
+// NOTE 
+// This function allows the player to attack the computer, attacks are chosen via buttons in our HTML.
+
+function playerAttack(result) {
+  if (result == 'slap'){
+   computer.health = (computer.health - 10)
+   console.log(computer.health)
+  }
+  if (result == 'kick'){
+   computer.health = (computer.health - 15)
+   console.log(computer.health)
+  }
+  if (result == 'punch'){
+   computer.health = (computer.health - 20)
+   console.log(computer.health)
+  }
+ }
