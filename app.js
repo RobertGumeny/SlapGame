6d7cc9844a4b649c395d74aaae7de31d9c1f2c99
Robert Gumeny <robert.gumeny528@gmail.com>
2020-03-19 16:49:16 -0600

    DONE! For now...

--- app.js
+++ app.js
@@ -23,32 +23,36 @@ let computer = {
 function playerAttack(result) {
   if (result == 'slap'){
    computer.health = (computer.health - 10)
-   document.getElementById("alert-bar").innerHTML = /*html*/ `<h1>You slapped the Computer for 10 damage!</h1>`
+   document.getElementById("alert-bar").innerHTML = /*html*/ `<h4>You slapped the Computer for 10 damage!</h4>`
   //  console.log(computer.health)
   let comHealthBar = document.getElementById("computer-health")
   comHealthBar.setAttribute("style", `width:${computer.health}%;`)
   }
   if (result == 'kick'){
    computer.health = (computer.health - 15)
-   document.getElementById("alert-bar").innerHTML = /*html*/ `<h1>You kicked the Computer for 15 damage!</h1>`
+   document.getElementById("alert-bar").innerHTML = /*html*/ `<h4>You kicked the Computer for 15 damage!</h4>`
    let comHealthBar = document.getElementById("computer-health")
    comHealthBar.setAttribute("style", `width:${computer.health}%;`)
   //  console.log(computer.health)
   }
   if (result == 'punch'){
    computer.health = (computer.health - 20)
-   document.getElementById("alert-bar").innerHTML = /*html*/ `<h1>You punched the Computer for 20 damage!</h1>`
+   document.getElementById("alert-bar").innerHTML = /*html*/ `<h4>You punched the Computer for 20 damage!</h4>`
    let comHealthBar = document.getElementById("computer-health")
    comHealthBar.setAttribute("style", `width:${computer.health}%;`)
   //  console.log(computer.health)
   }
-  randomComputerAttack()
-
+  if (computer.health > 0){
+    setTimeout(randomComputerAttack, 1000)
+  }
+ 
   if (player.health <= 0){
     computerWins()
+    setTimeout(resetGame, 2000)
   }
   if (computer.health <= 0){
     playerWins()
+    setTimeout(resetGame, 2000)
   }
 }
 // NOTE 
@@ -57,8 +61,8 @@ function playerAttack(result) {
 function playerWins(){
   player.wins++
   computer.losses++
-  document.getElementById("alert-bar").innerHTML = /*html*/ `<h1>Player Wins!</h1>`
-  document.getElementById("alert-bar-2").innerHTML = /*html*/ `<h1>Computer Loses!</h1>`
+  document.getElementById("alert-bar").innerHTML = /*html*/ `<h4>Player Wins!</h4>`
+  document.getElementById("alert-bar-2").innerHTML = /*html*/ `<h4>Computer Loses!</h4>`
   document.getElementById("player-wins").innerText = player.wins.toString()
   document.getElementById("computer-losses").innerText = computer.losses.toString()
   // @ts-ignore
@@ -75,8 +79,8 @@ function playerWins(){
 function computerWins(){
   computer.wins++
   player.losses++
-  document.getElementById("alert-bar").innerHTML = /*html*/ `<h1>Player Loses!</h1>`
-  document.getElementById("alert-bar-2").innerHTML = /*html*/ `<h1>Computer Wins!</h1>`
+  document.getElementById("alert-bar").innerHTML = /*html*/ `<h4>Player Loses!</h4>`
+  document.getElementById("alert-bar-2").innerHTML = /*html*/ `<h4>Computer Wins!</h4>`
   document.getElementById("player-losses").innerText = player.losses.toString()
   document.getElementById("computer-wins").innerText = computer.wins.toString()
 }
@@ -84,8 +88,8 @@ function computerWins(){
 // Script to execute to reset the game
 
 function resetGame(){
-  document.getElementById("alert-bar").innerHTML = /*html*/ `<h1>Start Slapping!</h1>`
-  document.getElementById("alert-bar-2").innerHTML = /*html*/ `<h1></h1>`
+  document.getElementById("alert-bar").innerHTML = /*html*/ `<h4>Start Slapping!</h4>`
+  document.getElementById("alert-bar-2").innerHTML = /*html*/ `<h4></h4>`
   player.health = 100;
   let playHealthBar = document.getElementById("player-health")
   playHealthBar.setAttribute("style",`width:${player.health}%;` )
@@ -109,23 +113,24 @@ function randomComputerAttack(){
   console.log(computerChoice)
   if (computerChoice == 'slap') {
     player.health = (player.health - 10)
-    document.getElementById("alert-bar-2").innerHTML = /*html*/ `<h1>The Computer slapped YOU for 10 damage!</h1>`
+    document.getElementById("alert-bar-2").innerHTML = /*html*/ `<h4>The Computer slapped YOU for 10 damage!</h4>`
     // document.getElementById("btn-comp-1").classList.add("active")
     let playHealthBar = document.getElementById("player-health")
     playHealthBar.setAttribute("style",`width:${player.health}%;` )
   }
   if (computerChoice == 'kick') {
     player.health = (player.health - 15)
-    document.getElementById("alert-bar-2").innerHTML = /*html*/ `<h1>The Computer kicked YOU for 15 damage!</h1>`
+    document.getElementById("alert-bar-2").innerHTML = /*html*/ `<h4>The Computer kicked YOU for 15 damage!</h4>`
     // document.getElementById("btn-comp-2").classList.add("active")
     let playHealthBar = document.getElementById("player-health")
     playHealthBar.setAttribute("style",`width:${player.health}%;` )
   }
   if (computerChoice == 'punch') {
     player.health = (player.health - 20)
-    document.getElementById("alert-bar-2").innerHTML = /*html*/ `<h1>The Computer punched YOU for 20 damage!</h1>`
+    document.getElementById("alert-bar-2").innerHTML = /*html*/ `<h4>The Computer punched YOU for 20 damage!</h4>`
     // document.getElementById("btn-comp-3").classList.add("active")
     let playHealthBar = document.getElementById("player-health")
     playHealthBar.setAttribute("style",`width:${player.health}%;` )
   }
 }
+
